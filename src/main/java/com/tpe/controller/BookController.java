f9c0c071bdbfdb8bff17742478c5bc725328ae11
Sefa Eyer <eyersefa@gmail.com>
2024-05-05 15:56:49 +0300

    Library w/springboot - create get delete

--- src/main/java/com/tpe/controller/BookController.java
+++ src/main/java/com/tpe/controller/BookController.java
@@ -1,10 +1,15 @@
 package com.tpe.controller;
 
+import com.tpe.domain.Book;
 import com.tpe.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController // Body,   springBoot da *->Controller yerine *->Restcontroller kullanilir
 //@Controller // Model , View
@@ -12,9 +17,51 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     @Autowired
-    private BookService bookService;
+    private BookService service;
 
 
+    //CREATE
+    //1- Save a Book & Return : Message
+    // http://localhost:8080/books + POST + JSON body
+    @PostMapping
+    public ResponseEntity<String> saveBook(@Valid @RequestBody Book book){
 
+        service.saveBook(book);
+
+        return new ResponseEntity<>("Kitap basariyla kaydedildi", HttpStatus.CREATED); //201
+    }
+
+
+    //READ
+    //2- Get All Books
+    // http://localhost:8080/books + GET
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks(){
+        List<Book> bookList= service.getAll();
+        return ResponseEntity.ok(bookList);//200
+    }
+
+
+    //3- Get a Book by its ID Return:Book
+    // http://localhost:8080/books/2 + GET
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable("id") Long identity){
+
+        Book found= service.getBookById(identity);
+
+            return new ResponseEntity<>(found, HttpStatus.OK);//200
+
+    }
+
+
+    //4- Delete a Book by its ID, Return : message
+    // http://localhost:8080/books/2 + DELETE
+    @DeleteMapping("/{no}")
+    public ResponseEntity<String> deleteBook(@PathVariable("no") Long id){
+
+        service.deleteBookById(id);
+
+        return ResponseEntity.ok("Kitap basariyla silindi. ID : "+id);
+    }
 
 }
--- src/main/java/com/tpe/service/BookService.java
+++ src/main/java/com/tpe/service/BookService.java
@@ -1,8 +1,12 @@
 package com.tpe.service;
 
+import com.tpe.domain.Book;
+import com.tpe.exceptions.BookNotFoundException;
 import com.tpe.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -10,4 +14,41 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+
+    //1-b
+    public void saveBook(Book book) {
+
+        //kontrol etmemiz gereken bir durum var mi?
+        bookRepository.save(book);
+
+    }
+
+    //2-b
+    public List<Book> getAll() {
+        return bookRepository.findAll();
+    }
+
+
+    //3-b
+    public Book getBookById(Long identity) {
+
+        //kontrol etmemiz gereken bir durum var mi?
+        Book book = bookRepository.findById(identity).
+                orElseThrow(()->new BookNotFoundException("Kitap bulunamadi, ID : "+identity));
+
+        return book;
+    }
+
+
+    //4-b
+    public void deleteBookById(Long id) {
+
+        //kontrol etmemiz gereken bir durum var mi?
+
+        //bu id ye ait bir kitap var mi
+        Book book= getBookById(id);
+        //bookRepository.delete(book);
+        bookRepository.deleteById(id);
+
+    }
 }
